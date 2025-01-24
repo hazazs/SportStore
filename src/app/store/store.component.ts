16ddfb2d1d3e8a35c3460253249cd14c9d335f8c
hazazs <hazafi.tibor@gmail.com>
2025-01-24 15:48:19 +0100

    Add selection by categories

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -7,17 +7,31 @@ import { Product } from "../model/product.model";
     templateUrl: "store.component.html"
 })
 export class StoreComponent {
+    selectedCategory: string | undefined;
+    
+    productsPerPage = 4;
+    selectedPage = 1;
 
     constructor(private repository: ProductRepository) {
 
     }
 
     get products(): Product[] {
-        return this.repository.getProducts();
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
     }
 
 }
